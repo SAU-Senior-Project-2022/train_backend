6a9a2a7bb135cce3a188ecfc346509259a21005e
Micah Tanner <micahrocz777@gmail.com>
2022-01-19 13:15:42 -0500

    Initial Commit in this branch

--- endpoints.py
+++ endpoints.py
@@ -1,4 +1,4 @@
-import database2
+import database
 import data
 import json
 from flask import jsonify, request, Response, Flask
@@ -16,7 +16,7 @@ class State(Resource):
         Resource (flask-restx.Resouce): imported from flask-restx
     """        
     def get(self, station_id):
-        resp = database2.getState(station_id)
+        resp = database.getState(station_id)
         # if (resp.get("error") != None):
         #     print(f"Error: {resp.get('error')}", file=stderr)
         return jsonify(json.loads(json.dumps(resp, cls=data.history.HistoryEncoder)))
@@ -33,7 +33,7 @@ class State(Resource):
             return Response(json.dumps({'error': "Missing json key: 'state'", 'correct': {"state": "true|false"}}), status=422, mimetype="application/json" )
             pass
         #print(bin(data.get('state')))
-        resp = database2.setState(station_id, data.get('state'))
+        resp = database.setState(station_id, data.get('state'))
         # if (resp.get("error") != None):
         #     print(f"Error: {resp.get('error')}", file=stderr)
         #     return Response(json.dumps({'error': resp.get('error'), 'correct': {"state": "true|false"}}), status=422, mimetype="application/json" )
@@ -46,7 +46,7 @@ class History(Resource):
         Resource (flask-restx.Resouce): imported from flask-restx
     """
     def get(self, station_id):
-        resp = database2.getHistory(station_id)
+        resp = database.getHistory(station_id)
         # if (resp[0].get('error') != None):
         #     print(f"Error: {resp[0].get('error')}", file=stderr)
         return jsonify(json.loads(json.dumps(resp, cls=data.history.HistoryEncoder)))
@@ -58,7 +58,7 @@ class LocationGet(Resource):
         Resource (flask-restx.Resouce): imported from flask-restx
     """
     def get(self, station_id):
-        resp = database2.getStation(station_id)
+        resp = database.getStation(station_id)
         # if (resp.get('error') != None):
         #     print(f"Error: {resp.get('error')}", file=stderr)
         return jsonify(json.loads(json.dumps(resp, cls=data.history.HistoryEncoder)))
@@ -76,7 +76,7 @@ class LocationPost(Resource):
         if ((data.get('latitude') == None) or (data.get('longitude') == None)):
             print("Error: All JSON fields expected not received", file=stderr)
             return Response(json.dumps({'error': "Did not receive all expected JSON fields", 'correct': {"latitude": "*location*", "longitude": "*location*"}}), status=422, mimetype="application/json" )
-        resp = database2.insert_new_station(data.get('latitude'), data.get('longitude'))
+        resp = database.insert_new_station(data.get('latitude'), data.get('longitude'))
         if (resp.get('error') != None):
             print(f"Error: {resp.get('error')}", file=stderr)
         return jsonify(resp)
